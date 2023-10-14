1cabc14dc686a65b4eb421c1e16ea41f9349bdcb
user.email <Vijayrakul003@gmail.com>
2023-10-14 16:29:00 +0530

    Added project

---
+++ LibraryManagement/LibraryManagement/Controllers/LibraryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
