6896b6e4b46597af98d72766bdcb33b074e21ed1
user.email <Vijayrakul003@gmail.com>
2023-10-14 22:45:21 +0530

    Added changes

--- LibraryManagement/LibraryManagement/Controllers/LibraryController.cs
+++ LibraryManagement/LibraryManagement/Controllers/LibraryController.cs
@@ -13,5 +13,10 @@ namespace LibraryManagement.Controllers
         {
             return View();
         }
+
+        public IActionResult AddBook()
+        {
+            return View();
+        }
     }
 }
